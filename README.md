dfc4c37ead20e83ca846f2e82798a38701af9715
Greg Althaus <galthaus@austin.rr.com>
2020-04-04 15:25:12 -0500

    First pass on something that NY times is doing better. :-)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # corona-view
 Corona Virus NY Times stats viewer
+
+## Contributers
+
+Greg Althaus
+
+## Data Source
+
+Data comes from: https://github.com/nytimes/covid-19-data
+
+## License and Attribution (replicated from NY times data site)
+
+In general, we are making this data publicly available for broad, noncommercial public use including by medical and public health researchers, policymakers, analysts and local news media.
+
+If you use this data, you must attribute it to “The New York Times” in any publication. If you would like a more expanded description of the data, you could say “Data from The New York Times, based on reports from state and local health agencies.”
+
+If you use it in an online presentation, we would appreciate it if you would link to our U.S. tracking page at [https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html](https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html).
+
+If you use this data, please let us know at covid-data@nytimes.com and indicate if you would be willing to talk to a reporter about your research.
+
+See our [LICENSE](LICENSE) for the full terms of use for this data.
+
+This license is co-extensive with the Creative Commons Attribution-NonCommercial 4.0 International license, and licensees should refer to that license ([CC BY-NC](https://creativecommons.org/licenses/by-nc/4.0/legalcode)) if they have questions about the scope of the license.
+
+
---
+++ script.js
@@ -0,0 +1,211 @@
+
+var all_data = {
+  usa: {
+    include_total: true, // true, false
+    math: "count",       // count, 3ema, ???
+    rangeX: { start: "2020-01-01", end: "3000-00-00" },
+    rangeY: "all", // all, top 10 on end, selector
+    states: [],
+    show: "cases",
+    lines: [],
+    layout: {},
+    sort_order: "alpha",
+    raw_data: {},
+    scale: "linear",
+    div: "usaDiv",
+    min: 100,
+},
+  county: {
+    include_total: true, // true, false
+    math: "count",       // count, 3ema, ???
+    rangeX: { start: "2020-01-01", end: "3000-00-00" },
+    rangeY: "all", // all, top 10 on end, selector
+    states: [],
+    counties: [],
+    show: "cases",
+    lines: [],
+    layout: {},
+    sort_order: "alpha",
+    raw_data: {},
+    scale: "linear",
+    div: "countyDiv",
+    min: 20,
+},
+};
+
+function rescale(g, elem) {
+        var config = all_data[g];
+        config.scale = elem.value;
+        config.layout.yaxis.type = (elem.selectedIndex ? "log" : "linear");
+        Plotly.newPlot(config.div, config.lines, config.layout);
+}
+
+function refunction(g, elem) {
+        var config = all_data[g];
+        config.math = elem.value;
+        parse_data(g);
+}
+
+function redata(g, elem) {
+        var config = all_data[g];
+        config.show = elem.value;
+        parse_data(g);
+}
+
+function resort(g, elem) {
+        var config = all_data[g];
+        config.sort_order = elem.value;
+        parse_data(g);
+}
+
+function sort_lines(lines, method) {
+        lines.sort(function(a, b) {
+                if (method == "most") {
+                        return b.y[b.y.length-1] - a.y[a.y.length-1];
+                }
+                if (method == "least") {
+                        return a.y[a.y.length-1] - b.y[b.y.length-1];
+                }
+                if (a.name < b.name) {
+                        return -1;
+                }
+                if (a.name > b.name) {
+                        return 1;
+                }
+                return 0;
+        });
+
+        return lines;
+}
+
+
+function parse_data(table) {
+        var config = all_data[table]
+        var raw_data = all_data[table].raw_data
+
+        var lines = {};
+        var first, last;
+
+        var dateIdx = 0;
+        var nameIdx = 1;
+        var countIdx = 3;
+        if (table == "county") {
+                countIdx += 1;
+        }
+        if (config.show == "deaths") {
+                countIdx += 1;
+        }
+
+        for (i = 1; i < raw_data.length; i++) {
+                var sdata = raw_data[i]
+                if ((sdata[dateIdx] >= config.rangeX.start) && (sdata[dateIdx] <= config.rangeX.end)) {
+                        var nd = parseInt(sdata[countIdx]);
+                        // Skip entries until min met.
+                        if (nd < config.min) {
+                                continue
+                        }
+
+                        if (first === undefined) {
+                                first = sdata[dateIdx];
+                        }
+                        last = sdata[dateIdx];
+                        var state_data = lines[sdata[nameIdx]] || { name: sdata[nameIdx], type: "scatter", mode: "lines", x: [], y: [], r_y: []};
+
+                        state_data.x.push(sdata[dateIdx]);
+
+                        state_data.r_y.push(nd);
+                        switch (config.math) {
+                        case "count":
+                                state_data.y.push(nd);
+                                break;
+                        case "3ema-count":
+                                var prev = 0;
+                                if (state_data.y.length > 1) {
+                                        prev = state_data.y[state_data.y.length-1];
+                                }
+                                var ema = (nd * 0.5) + (prev * 0.5);
+                                state_data.y.push(ema);
+                                break;
+                        case "diff":
+                                var rprev = 0;
+                                if (state_data.r_y.length > 1) {
+                                        rprev = state_data.r_y[state_data.y.length-1];
+                                }
+                                nd = nd - rprev;
+                                state_data.y.push(nd);
+                                break;
+                        case "3ema-diff":
+                                var prev = 0;
+                                var rprev = 0;
+                                if (state_data.r_y.length > 1) {
+                                        rprev = state_data.r_y[state_data.y.length-1];
+                                        prev = state_data.y[state_data.y.length-1];
+                                }
+                                nd = nd - rprev;
+                                var ema = (nd / 3) + (prev * 2 / 3);
+                                state_data.y.push(ema);
+                                break;
+                        }
+
+                        lines[sdata[nameIdx]] = state_data;
+                }
+        }
+        lines = Object.values(lines)
+        lines = sort_lines(lines, config.sort_order)
+        config.lines = lines;
+
+        config.layout = {
+  title: 'United States Per State Data for COVID-19 ' + config.show,
+  barmode: 'group',
+  xaxis: {
+    autorange: true,
+    range: [first, last],
+    tickangle: -45,
+    rangeselector: {buttons: [
+        {
+          count: 1,
+          label: '1d',
+          step: 'day',
+          stepmode: 'backward'
+        },
+        {
+          count: 7,
+          label: '1w',
+          step: 'week',
+          stepmode: 'backward'
+        },
+        {step: 'all'}
+      ]},
+    rangeslider: {range: [first, last]},
+    type: 'date'
+  },
+  yaxis: {
+    autorange: true,
+    type: config.scale
+  }
+};
+        Plotly.newPlot(config.div, config.lines, config.layout);
+}
+
+$.ajax({
+  type: "GET",
+  url: "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv",
+  dataType: "text",
+  success: function(response)
+  {
+	all_data.usa.raw_data = $.csv.toArrays(response);
+        parse_data("usa");
+  }
+});
+
+$.ajax({
+  type: "GET",
+  url: "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv",
+  dataType: "text",
+  success: function(response)
+  {
+	all_data.county.raw_data = $.csv.toArrays(response);
+        parse_data("county");
+  }
+});
+
